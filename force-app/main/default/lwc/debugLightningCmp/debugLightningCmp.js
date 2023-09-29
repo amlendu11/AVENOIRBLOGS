af49e8792d485eb4c9618541a172bbbca7bb56b5
SantuCodeX <santosh.avenoir@gmail.com>
2023-09-29 11:47:36 +0530

    updated display and edit

--- force-app/main/default/lwc/debugLightningCmp/debugLightningCmp.js
+++ force-app/main/default/lwc/debugLightningCmp/debugLightningCmp.js
@@ -1,15 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import { getRecord } from "lightning/uiRecordApi"
+
+const FIELDS = ["Contact.Name", "Contact.Phone","contact.Email"];
 
 export default class DebugLightningCmp extends LightningElement {
-    handleSubmit(event) {
-        event.preventDefault();
-        const fields = event.detail.fields;
-        this.template.querySelector('lightning-record-edit-form').submit(fields);
+
+    recordId = "003H4000001jJqiIAE";
+    contactData;
+    error;
+
+
+    @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
+    wiredRecord({ error, data }) {
+        if(data) {
+            console.log(JSON.stringify(data));
+            this.contactData = data;
+        }else if(error) {
+            console.log(JSON.stringify(error));
+            this.error = error;
+        }
     }
+
+    //Edit form 
     
+    handleSubmit(event) {
+        event.preventDefault();      
+        const fields = event.detail.fields;
+        console.log(JSON.stringify(fields));
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+        console.log('onsubmit event recordEditForm'+ event.detail.fields);
+    }
+
+    handleSuccess(event) {
+        console.log(JSON.stringify(event));
+        console.log('onsuccess event recordEditForm', event.detail.id);
+    }
+
     handleError(event) {
-        const errorMessages = event.detail.detail.error.body.message;
-        console.log(errorMessages)
+        console.log(JSON.stringify(event));
+        console.log('SomeError Occur');
     }
     
 }
