af2460e4e924473c61e2725970087109ae60eab7
NikhilMehra29 <Nikhilmehra9a@gmail.com>
2023-10-13 19:50:46 +0530

    how to add field path LWC

---
+++ force-app/main/default/lwc/sfLightningPath/sfLightningPath.js
@@ -0,0 +1,30 @@
+/***********************************************************************
+* (c) Copyright 2023 Avenoir Technologies Pvt. Ltd. All rights reserved.
+* Created by: Nikhil Mehra
+* Ticket Number: AVEBLOG59
+------------------------------------------------------------------------
+* Blog: How to add field-path like opportunity in LWC Component
+------------------------------------------------------------------------
+* Version History:
+* VERSION    DEVELOPER NAME        DATE         DETAIL FEATURES
+   1.0       Nikhil Mehra       08/10/2023      Initial Development
+***********************************************************************/
+import { LightningElement,api} from 'lwc';
+import { pathItems, pathElements } from 'c/paths';
+export default class SfLightningPath extends LightningElement {
+    @api currentState = '';
+    @api steps;
+    @api currentStepIndex;
+
+    pathElements = pathElements;
+    pathHandler(event)
+    {
+        if(this.pathElements.indexOf(this.currentState)>this.pathElements.indexOf(event.currentTarget.value)) {
+            this.currentState = event.currentTarget.value;
+            this.dispatchEvent(new CustomEvent('selectedpath', {
+                detail: this.currentState
+            }));
+        }
+        
+    }
+}
