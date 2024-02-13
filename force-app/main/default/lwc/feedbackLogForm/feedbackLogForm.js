4cd111872c2ce2e4ad6cb41bb23c0b2761f0a9e4
AvantikaTikale <avantika.avenoir@gmail.com>
2024-02-13 13:28:55 +0530

    MONITOR LOG

--- force-app/main/default/lwc/feedbackLogForm/feedbackLogForm.js
+++ force-app/main/default/lwc/feedbackLogForm/feedbackLogForm.js
@@ -1,22 +1,31 @@
-import { LightningElement } from 'lwc';
-import { log } from 'lightning/logger';
+import {LightningElement} from 'lwc';
+import {log} from 'lightning/logger';
 import userId from '@salesforce/user/Id'; 
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import TITLE from '@salesforce/label/c.TITLE';
+import YOUR_FEEDBACK from '@salesforce/label/c.YOUR_FEEDBACK';
+import FEEDBACK_NAME from '@salesforce/label/c.FEEDBACK_NAME';
+import SUBMIT from '@salesforce/label/c.SUBMIT';
 
 export default class FeedbackForm extends LightningElement {
+    label = {
+        TITLE,
+        YOUR_FEEDBACK,
+        FEEDBACK_NAME,
+        SUBMIT
+    };
     handleSubmit() {
         let feedback = this.template.querySelector('lightning-input').value;
-
         let logData = {
             type: "feedback_submission",
             userId: userId,
             timestamp: new Date().toISOString(),
             feedback: feedback
         };
-
         log(logData);
         this.showToast('Success', 'Feedback submitted successfully', 'success');
     }
+    
     showToast(title, message, variant) {
         const event = new ShowToastEvent({
             title: title,
